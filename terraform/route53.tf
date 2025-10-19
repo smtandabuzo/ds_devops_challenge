18e5baad5ab08e6b5779085be51608e6c7281531
sazi <sazi.mtandabuzo@ayo4u.com>
2025-10-19 17:39:32 +0200

    Task 4. Fix: Adjust to use namespace ID which is more reliable, add route 53 to the Terraform configuration.

---
+++ terraform/route53.tf
@@ -0,0 +1,19 @@
+# Route 53 Private Hosted Zone for service discovery
+resource "aws_route53_zone" "private" {
+  name = "${var.app_name}.local"
+  
+  vpc {
+    vpc_id = local.vpc_id
+  }
+
+  tags = {
+    Environment = var.environment
+    Application = var.app_name
+  }
+}
+
+# Output the zone ID for reference
+output "route53_zone_id" {
+  description = "The ID of the Route 53 private hosted zone"
+  value       = aws_route53_zone.private.zone_id
+}
