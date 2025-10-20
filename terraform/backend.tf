cfa2957afbad8fac6b9873174a1641aeb460994f
sazi <sazi.mtandabuzo@ayo4u.com>
2025-10-20 10:46:59 +0200

    Task 4. Use existing bucket in correct region

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "ds-devops-tfstate-810772959397"
     key            = "terraform.tfstate"
-    region = "us-east-2"
+    region = "us-east-1"
     encrypt        = true
   }
 }
